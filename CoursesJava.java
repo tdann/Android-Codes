e803409c21d63229138bbd212a42153e2170863f
tdann <32097685+tdann@users.noreply.github.com>
2021-05-13 12:42:52 +0200

    Add files via upload
    This is my first course work

---
+++ CoursesJava.java
@@ -0,0 +1,12 @@
+package com.example.helloword;
+public class CoursesJava
+{
+public static void main(String[] args){
+        System.out.println("CSC3115 Mobile Programming");
+        System.out.println("CSC3117 Automata, Complexity and Computability");
+        System.out.println("CSC3112 Data Communications and Networks");
+        System.out.println("CSC3113 Entreprenuership");
+        System.out.println("CSC3104 Computer Networks and Security");
+        System.out.println("CSC3116 Artificial Intelligence");
+                }
+        }
---
+++ courses.kt
@@ -0,0 +1,9 @@
+package com.example.helloword
+
+fun main(args: Array<String>){
+
+    var Year3Courses: Array<String> = arrayOf("CSC3115 MobileProgramming, CSC3113 Entreprenuership, CSC3116 Artificial Intelligence, CSC3112 Data Communications and Networks, CSC3104 Computer Networks and Security, CSC3117 Automata Complexity and Computability")
+
+    Year3Courses.forEach{System.out.print(it)}
+
+}
